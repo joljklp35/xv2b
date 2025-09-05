b543a63e0da06c79b949e94d19d01e10481180ad
joljklp35 <joljklp35@gmail.com>
2025-09-05 13:20:48 +0800

    Update horizon.php

--- config/horizon.php
+++ config/horizon.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'path' => config('v2board.secure_path',config('v2board.frontend_admin_path', hash('crc32b', config('app.key')))) . '/horizon',
+    'path' => 'horizon',
     
     /*
     |--------------------------------------------------------------------------
