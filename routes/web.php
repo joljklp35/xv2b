60904899f1a8479fa45ba32b900081d11cfbb0fb
daley7292 <daley7292@gmail.com>
2025-09-08 17:01:58 +0800

    x

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,29 @@
 use App\Services\ThemeService;
 use Illuminate\Http\Request;
 
+Route::get('/', function (Request $request) {
+    if (config('v2board.app_url') && config('v2board.safe_mode_enable', 0)) {
+        if ($request->server('HTTP_HOST') !== parse_url(config('v2board.app_url'))['host']) {
+            abort(403);
+        }
+    }
+    $renderParams = [
+        'title' => config('v2board.app_name', 'V2Board'),
+        'theme' => config('v2board.frontend_theme', 'default'),
+        'version' => config('app.version'),
+        'description' => config('v2board.app_description', 'V2Board is best'),
+        'logo' => config('v2board.logo')
+    ];
+
+    if (!config("theme.{$renderParams['theme']}")) {
+        $themeService = new ThemeService($renderParams['theme']);
+        $themeService->init();
+    }
+
+    $renderParams['theme_config'] = config('theme.' . config('v2board.frontend_theme', 'default'));
+    return view('theme::' . config('v2board.frontend_theme', 'default') . '.dashboard', $renderParams);
+});
+
 Route::get('/' . config('v2board.secure_path', config('v2board.frontend_admin_path', hash('crc32b', config('app.key')))), function (Request $request) {
     $host = $request->header('x-forwarded-host', $request->server('HTTP_HOST'));
     if ($wh = config('v2board.whitehost')) {
